478eda02ffffa0fdf57697f38aeff787ff18f78f
sebas <sebas@DESKTOP-PTO48N5>
2020-05-28 21:06:30 -0300

    ASUNTO: Inicialización del Proyecto. AUTOR: Sebastián Darío Diaz. TASK00: Inicialización del Proyecto TP5.

---
+++ src/main/java/ar/edu/unju/fi/Diaz3283Tp5Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Diaz3283Tp5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Diaz3283Tp5Application.class, args);
+	}
+
+}
